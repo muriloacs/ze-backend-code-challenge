52ada1efc61a9da997bd7484ded3882abdd7d84f
Murilo <murilo@apprl.com>
2020-07-13 16:40:37 -0300

    45GJY improving partner model

--- ze/partner/models.py
+++ ze/partner/models.py
@@ -4,9 +4,9 @@ from django.contrib.gis.db import models
 
 
 class Partner(models.Model):
-    trading_name = models.CharField(max_length=100, blank=False, null=False)
-    owner_name = models.CharField(max_length=100, blank=False, null=False)
-    document = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    trading_name = models.CharField(max_length=100, blank=False)
+    owner_name = models.CharField(max_length=100, blank=False)
+    document = models.CharField(max_length=50, blank=False, unique=True)
     coverage_area = models.MultiPolygonField(srid=4326, null=False)
     address = models.PointField(null=False)
 
