0b5b7c7a74f8920d52d721bcec12b604d69ca25d
Murilo <murilo@apprl.com>
2020-07-15 22:36:25 -0300

    YUP5L make spatial_index explicit on partner model

--- ze/partner/models.py
+++ ze/partner/models.py
@@ -10,7 +10,9 @@ class Partner(models.Model):
     owner_name = models.CharField(max_length=100, blank=False)
     document = models.CharField(max_length=50, blank=False, unique=True)
     coverage_area = models.MultiPolygonField(srid=4326, null=False, spatial_index=True)
-    address = models.PointField(null=False, spatial_index=True)  # creates a spatial index for the given geometry field
+    address = models.PointField(null=False, spatial_index=True)
+    # Creates a spatial index for the given geometry field. Default is True, just making it explicit.
+
     objects = PartnerQuerySet.as_manager()
 
     def __str__(self):
